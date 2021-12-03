caeac62aa56353386a30d556e1a8a181a6e02793
David Taylor <david@taylorhq.com>
2021-12-03 17:57:03 +0000

    Migrate plugin to ManagedAuthenticator pattern (#18)
    https://meta.discourse.org/t/94691

--- plugin.rb
+++ plugin.rb
@@ -1,40 +1,20 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-linkedin-auth
+# name: discourse-linkedin-auth
 # about: Enable Login via LinkedIn
-# version: 0.0.3
+# version: 1.0
 # authors: Matthew Wilkin
-# url: https://github.com/cpradio/discourse-plugin-linkedin-auth
-
-require 'auth/oauth2_authenticator'
+# url: https://github.com/discourse/discourse-linkedin-auth
 
 gem 'omniauth-linkedin-oauth2', '1.0.0'
 
-register_svg_icon "fab-linkedin-in" if respond_to?(:register_svg_icon)
-register_asset 'stylesheets/linkedin-login.scss'
+register_svg_icon "fab-linkedin-in"
 
 enabled_site_setting :linkedin_enabled
 
-class LinkedInAuthenticator < ::Auth::OAuth2Authenticator
-  PLUGIN_NAME = 'oauth-linkedin'
-
+class ::LinkedInAuthenticator < ::Auth::ManagedAuthenticator
   def name
     'linkedin'
-  end
-
-  def after_authenticate(auth_token)
-    result = super
-
-    if result.user && result.email && (result.user.email != result.email)
-      begin
-        result.user.primary_email.update!(email: result.email)
-      rescue
-        used_by = User.find_by_email(result.email)&.username
-        Rails.logger.warn("FAILED to update email for #{user.username} to #{result.email} cause it is in use by #{used_by}")
-      end
-    end
-
-    result
   end
 
   def register_middleware(omniauth)
@@ -51,11 +31,5 @@ class LinkedInAuthenticator < ::Auth::OAuth2Authenticator
   end
 end
 
-auth_provider frame_width: 920,
-              frame_height: 800,
-              icon: 'fab-linkedin-in',
-              authenticator: LinkedInAuthenticator.new(
-                'linkedin',
-                trusted: true,
-                auto_create_account: true
-              )
+auth_provider authenticator: ::LinkedInAuthenticator.new,
+              icon: 'fab-linkedin-in'

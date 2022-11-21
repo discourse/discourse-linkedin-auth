78ffdcbcdb47196c05ce74a2e00cb2ee1bfa0926
OsamaSayegh <asooomaasoooma90@gmail.com>
2022-11-21 16:12:50 +0300

    DEV: Explicitly define primary_email_verified? method for LinkedIn authenticators

--- plugin.rb
+++ plugin.rb
@@ -31,6 +31,13 @@ class ::LinkedInAuthenticator < ::Auth::ManagedAuthenticator
   def enabled?
     SiteSetting.linkedin_enabled
   end
+
+  # linkedin doesn't let users login with OAuth2 to websites unless they verify
+  # their email address so whatever email we get from linkedin has to be
+  # verified
+  def primary_email_verified?(auth_token)
+    true
+  end
 end
 
 auth_provider authenticator: ::LinkedInAuthenticator.new,

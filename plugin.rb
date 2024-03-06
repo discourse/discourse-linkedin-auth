9fada8465cf32aa41d1a73274438c823df194fe5
Loïc Guitaut <loic@discourse.org>
2024-03-06 11:57:14 +0100

    DEV: Fix new Rubocop offenses

--- plugin.rb
+++ plugin.rb
@@ -15,31 +15,6 @@ register_asset "stylesheets/common.scss"
 
 enabled_site_setting :linkedin_enabled
 
-class ::LinkedInAuthenticator < ::Auth::ManagedAuthenticator
-  def name
-    "linkedin"
-  end
-
-  def register_middleware(omniauth)
-    omniauth.provider :linkedin,
-                      setup:
-                        lambda { |env|
-                          strategy = env["omniauth.strategy"]
-                          strategy.options[:client_id] = SiteSetting.linkedin_client_id
-                          strategy.options[:client_secret] = SiteSetting.linkedin_secret
-                        }
-  end
-
-  def enabled?
-    SiteSetting.linkedin_enabled
-  end
-
-  # linkedin doesn't let users login with OAuth2 to websites unless they verify
-  # their email address so whatever email we get from linkedin has to be
-  # verified
-  def primary_email_verified?(auth_token)
-    true
-  end
-end
+require_relative "lib/linked_in_authenticator"
 
 auth_provider authenticator: ::LinkedInAuthenticator.new, icon: "fab-linkedin"
